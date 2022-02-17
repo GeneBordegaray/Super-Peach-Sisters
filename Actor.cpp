c67383a93b44f400c8bfc98cde6f9529ee7e5e69
GeneBordegaray <57379524+GeneBordegaray@users.noreply.github.com>
2022-02-17 10:16:07 -0800

    Update Actor.cpp

--- Actor.cpp
+++ Actor.cpp
@@ -6,7 +6,9 @@
 /*****Base Actor*****/
 Actor::Actor(StudentWorld* world, int imageID, int startX, int startY, int startDirection, int depth, double size)
 	:GraphObject(imageID, startX, startY, startDirection, depth, size)
-{}
+{
+	m_world = world;
+}
 Actor::~Actor()
 {}
 
@@ -54,28 +56,27 @@ int Peach::getHP()
 
 void Peach::doSomething()
 {
-	bool alive = isAlive();
-	if (alive)
+	
+	int keyPress;
+	if (getWorld()->getKey(keyPress))
 	{
 
-		int keyPress;
-		if (getWorld()->getKey(keyPress))
+		switch (keyPress)
 		{
+		case KEY_PRESS_LEFT:
+			setDirection(left);
+			moveTo(getX() - 4, getY());
+			break;
 
-			switch (keyPress)
-			{
+		case KEY_PRESS_RIGHT:
+			setDirection(right);
+			moveTo(getX() + 4, getY());
+			break;
 
-			case KEY_PRESS_LEFT:
-				setDirection(left);
-				moveTo(getX() - 4, getY());
-				break;
-
-			case KEY_PRESS_RIGHT:
-				setDirection(right);
-				moveTo(getX() + 4, getY());
-				break;
-			}
+		default:
+			break;
 		}
+	
 	}
-	return;
+return;
 }
