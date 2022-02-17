10a4a33d09737442e356c5417869b9e0390fc891
GeneBordegaray <57379524+GeneBordegaray@users.noreply.github.com>
2022-02-16 22:35:01 -0800

    Create StudentWorld.h

---
+++ StudentWorld.h
@@ -0,0 +1,32 @@
+#ifndef STUDENTWORLD_H_
+#define STUDENTWORLD_H_
+
+#include "GameWorld.h"
+#include "Actor.h"
+#include "Level.h"
+#include <string>
+#include <vector>
+
+// Students:  Add code to this file, StudentWorld.cpp, Actor.h, and Actor.cpp
+
+class StudentWorld : public GameWorld
+{
+public:
+    StudentWorld(std::string assetPath);
+    ~StudentWorld();
+
+    virtual int init();
+    virtual int move();
+    virtual void cleanUp();
+
+    void createLevel();
+
+private:
+    //maintain the list of actors: //blocks
+    Peach* m_peach;
+
+   std:: vector<Actor*> actorList;
+};
+
+
+#endif // STUDENTWORLD_H_
