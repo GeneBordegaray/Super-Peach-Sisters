612e600d889e305a3db6cbbb3806d97e938ce3bb
GeneBordegaray <57379524+GeneBordegaray@users.noreply.github.com>
2022-02-25 14:57:33 -0800

    Update StudentWorld.cpp

--- StudentWorld.cpp
+++ StudentWorld.cpp
@@ -1,170 +1,35 @@
-#include "StudentWorld.h"
-#include "GameConstants.h"
+#ifndef STUDENTWORLD_H_
+#define STUDENTWORLD_H_
+
+#include "GameWorld.h"
+#include "Actor.h"
+#include "Level.h"
 #include <string>
-#include <sstream>
-#include <iomanip>
-using namespace std;
+#include <vector>
 
-GameWorld* createStudentWorld(string assetPath)
+// Students:  Add code to this file, StudentWorld.cpp, Actor.h, and Actor.cpp
+
+class StudentWorld : public GameWorld
 {
-    return new StudentWorld(assetPath);
-}
+public:
+    StudentWorld(std::string assetPath);
+    ~StudentWorld();
 
-// Students:  Add code to this file, StudentWorld.h, Actor.h, and Actor.cpp
+    virtual int init();
+    virtual int move();
+    virtual void cleanUp();
 
-StudentWorld::StudentWorld(string assetPath)
-    : GameWorld(assetPath)
-{
-    m_peach = nullptr;
-}
-StudentWorld::~StudentWorld()
-{
-    cleanUp();
-}
+    bool createLevel(int lev);
 
+    bool overlap(double ax, double ay, double bx, double by) const;
+    bool canMoveThere(Actor* player, double ax, double ay) const;
 
-int StudentWorld::init()
-{
-    if (getLevel() > 99)
-    {
-        return GWSTATUS_PLAYER_WON;
-    }
-    if (createLevel(getLevel()) == false)
-    {
-        return GWSTATUS_LEVEL_ERROR;
-    }
+private:
+    //maintain the list of actors: //blocks
+    Peach* m_peach;
 
-    return GWSTATUS_CONTINUE_GAME;
-}
+    std::vector<Actor*> actorList;
+};
 
 
-int StudentWorld::move()
-{
-    // This code is here merely to allow the game to build, run, and terminate after you hit enter.
-    // Notice that the return value GWSTATUS_PLAYER_DIED will cause our framework to end the current level.
-    if (m_peach->isAlive() == true)
-    {
-        m_peach->doSomething();
-    }
-    else
-    {
-        return GWSTATUS_PLAYER_DIED;
-    }
-
-    vector<Actor*>::iterator it;
-    for (it = actorList.begin(); it != actorList.end(); it++) //telling all actors in vector to do something
-    {
-        (*it)->doSomething();
-    }
-
-    // decLives();
-
-    return GWSTATUS_CONTINUE_GAME;
-}
-
-void StudentWorld::cleanUp()
-{
-    delete m_peach; //delteing main character
-    m_peach = nullptr;
-
-    vector<Actor*>::iterator it;
-    for (it = actorList.begin(); it != actorList.end(); it++) //deleteing all actors in vector
-    {
-        delete* it;
-    }
-    actorList.clear(); //clearing vector
-}
-
-bool StudentWorld::createLevel(int lev)
-{
-    actorList.clear();
-
-    stringstream str;
-    str << "level" << setfill('0') << setw(2) << lev; //setting proper level number
-    string str2 = str.str();
-
-    Level curLev(assetPath());
-    Level::LoadResult result = curLev.loadLevel(str2 + ".txt"); //getting proper level contents
-
-    if (result == Level::load_fail_file_not_found)
-    {
-        cerr << "Could not find level01.txt data file" << endl;
-        return false;
-    }
-    else if (result == Level::load_fail_bad_format)
-    {
-        cerr << "level01.txt is improperly formatted" << endl;
-        return false;
-    }
-    else if (result == Level::load_success)
-    {
-        cerr << "Successfully loaded level" << endl;
-        Level::GridEntry mapEntry;
-        for (int x = 0; x < VIEW_WIDTH; x++) //iterating through text file
-            for (int y = 0; y < VIEW_HEIGHT; y++)
-            {
-                mapEntry = curLev.getContentsOf(x, y);
-                //making new actors based off the charaters found in the level.txt file
-                Actor* ptr;
-                switch (mapEntry)
-                {
-                case Level::empty:
-                    break;
-
-                case Level::peach:
-                    m_peach = new Peach(this, IID_PEACH, SPRITE_WIDTH * x, SPRITE_HEIGHT * y, 0, 0, 1.0);
-                    break;
-
-                case Level::block:
-                case Level::flower_goodie_block:
-                case Level::mushroom_goodie_block:
-                case Level::star_goodie_block:
-                    ptr = new Block(this, IID_BLOCK, SPRITE_WIDTH * x, SPRITE_HEIGHT * y, 0, 2, 1.0);
-                    actorList.push_back(ptr);
-                    break;
-
-                case Level::pipe:
-                    ptr = new Pipe(this, IID_PIPE, SPRITE_WIDTH * x, SPRITE_HEIGHT * y, 0, 2, 1.0);
-                    actorList.push_back(ptr);
-                    break;
-
-                }
-            }
-        return true;
-    }
-    return false;
-}
-
-
-bool StudentWorld::overlap(double ax, double ay, double bx, double by) const
-{
-    if (abs(ax - bx) < SPRITE_WIDTH && abs(ay - by) < SPRITE_HEIGHT) //checking overlapping between the two actor parameters
-    {
-        return true;
-    }
-    return false;
-}
-
-bool StudentWorld::canMoveThere(Actor* player, double ax, double ay) const
-{
-    vector<Actor*>::const_iterator it;
-    for (it = actorList.begin(); it != actorList.end(); it++) //iterating through actor vector
-    {
-        //get the object that it is pointing to's coords
-        double bx = (*it)->getX();
-        double by = (*it)->getY();
-
-        if ((*it) == player) //check if it is pointing at the peach
-        {
-            return false;
-        }
-        if ((*it)->canBlock())
-        {
-            if (overlap(ax, ay, bx, by))
-            {
-                return false;
-            }
-        }
-    }
-    return true;
-}
+#endif // STUDENTWORLD_H_
