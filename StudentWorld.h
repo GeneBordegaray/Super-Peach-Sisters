02731d948e2f9edf016fa35cfeafa5ffdb8b5f8c
GeneBordegaray <57379524+GeneBordegaray@users.noreply.github.com>
2022-02-26 12:43:30 -0800

    Update StudentWorld.h

--- StudentWorld.h
+++ StudentWorld.h
@@ -1,60 +1,187 @@
-#ifndef STUDENTWORLD_H_
-#define STUDENTWORLD_H_
+#ifndef ACTOR_H_
+#define ACTOR_H_
 
-#include "GameWorld.h"
-#include "Actor.h"
-#include "Level.h"
-#include <string>
-#include <vector>
+#include "GraphObject.h"
+// Students:  Add code to this file, Actor.cpp, StudentWorld.h, and StudentWorld.cpp
+class StudentWorld;
 
-// Students:  Add code to this file, StudentWorld.cpp, Actor.h, and Actor.cpp
-
-class StudentWorld : public GameWorld
+/*****Base Actor*****/
+class Actor : public GraphObject
 {
 public:
-    StudentWorld(std::string assetPath);
-    ~StudentWorld();
 
-    virtual int init();
-    virtual int move();
-    virtual void cleanUp();
+	Actor(StudentWorld* world, int imageID, int startX, int startY, int startDirection, int depth, double size);
+	virtual ~Actor();
 
-    bool createLevel(int lev);
+	//what to do each tick
+	void doSomething();
 
-    //add an actor to the world
-    void addGoodie(Block* a);
+	//get bonked
+	virtual void bonk();
 
-    //are two actors touching
-    bool overlap(double ax, double ay, double bx, double by) const;
+	//is this a character that prevents movement
+	virtual bool canBlock() const;
 
-    //is an object touching peach
-    bool overlapPeach(Actor* a) const;
-    bool overlapPeach() const;
+	virtual int getGoodieType() const;
 
-    //can peach move there
-    bool canMoveThere(Actor* player, double ax, double ay) const;
+	//what to do when you are supposed to get damaged
+	virtual void sufferDamage();
 
-    //should actor move there or bonk it
-    bool moveOrBonk(Actor* player, double ax, double ay) const;
+	//is the character alive
+	bool isAlive() const;
+	void setDead();
 
-    //bonk overlapping characters with peach
-    bool bonkOverlappingPeach(Actor* bonker) const;
+	//turn the character around
+	void reverseActor();
 
-    //bonk other things
-    //Peach is always the bonker
-    bool bonkOverlappingActor(Actor* bonker) const;
-
-    //Setting peach hp
-    void setPeachHP(int hp) const;
+	StudentWorld* getWorld() const;
 
 private:
-    //my peach character
-    Peach* m_peach;
-    int m_hp;
+	virtual void doSomethingUnique() = 0;
+	StudentWorld* m_world;
+	bool m_alive;
+};
 
-    //keep track of all the actors in the game
-    std::vector<Actor*> actorList;
+/*****Stationary Actor Base Class*****/
+class stationaryActors : public Actor
+{
+public:
+	stationaryActors(StudentWorld* world, int imageID, int startX, int startY, int startDirection, int depth, double size);
+	virtual ~stationaryActors();
+
+	virtual bool canBlock() const;
 };
 
 
-#endif // STUDENTWORLD_H_
+
+/*****Bad Guy Actor Class*****/
+class BadGuy : public Actor
+{
+public:
+	BadGuy(StudentWorld* world, int imageID, int startX, int startY, int startDirection, int depth, double size);
+	virtual ~BadGuy();
+
+
+private:
+
+};
+
+
+
+
+/*****Goodie Base Class*****/
+class Goodie : public Actor
+{
+public:
+	Goodie(StudentWorld* world, int imageID, int startX, int startY, int startDirection, int depth, double size);
+	virtual ~Goodie();
+
+private:
+
+};
+
+
+
+
+/*****Block*****/
+class Block : public stationaryActors
+{
+public:
+	//blocks can have different goodies in them
+	enum GoodieType {none, flower, mushroom, star};
+
+
+	Block(StudentWorld* world, int imageID, int startX, int startY, int startDirection, int depth, double size, GoodieType g);
+	virtual ~Block();
+
+	//what type of goodie is in the block
+	virtual int getGoodieType() const;
+
+	virtual void bonk();
+
+private:
+	int m_goodieCount;
+	//1 is mushroom, 2 is flower, 3 is start, 0 is nothing
+	int m_goodieType;
+	virtual void doSomethingUnique();
+};
+
+
+/*****Pipes*****/
+class Pipe : public stationaryActors
+{
+public:
+
+	Pipe(StudentWorld* world, int imageID, int startX, int startY, int startDirection, int depth, double size);
+	virtual ~Pipe();
+
+	virtual void bonk();
+
+private:
+	virtual void doSomethingUnique();
+};
+
+/*****Peach Class*****/
+class Peach : public Actor
+{
+public:
+
+	Peach(StudentWorld* world, int imageID, int startX, int startY, int startDirection, int depth, double size);
+	virtual ~Peach();
+
+	//Peach has health
+	void setHP(int hp);
+	int getHP();
+	void decHP();
+
+	//Peach can jump
+	int getJumpPower();
+	void setJumpPower(int num);
+	void decJumpPower();
+
+	//peach needs to be invincible for some periods of time
+	void setInvincible(int remaining_invincible);
+	int getInvincible();
+	void decInvincible();
+	bool isInvincible() const;
+
+	virtual void bonk();
+
+private:
+	virtual void doSomethingUnique();
+	int m_hp;
+	int remaining_jump_power;
+	int remaining_invincbile;
+};
+
+
+/*****Goomba Class*****/
+class Goomba : public BadGuy
+{
+public:
+	Goomba(StudentWorld* world, int imageID, int startX, int startY, int startDirection, int depth, double size);
+	virtual ~Goomba();
+
+	//goombas bonk
+	virtual void bonk();
+
+private:
+	virtual void doSomethingUnique();
+};
+
+
+
+/*****Mushroom Class*****/
+class Mushroom : public Goodie
+{
+public:
+	Mushroom(StudentWorld* world, int imageID, int startX, int startY, int startDirection, int depth, double size);
+	virtual ~Mushroom();
+
+private:
+	virtual void doSomethingUnique();
+};
+
+
+
+#endif // ACTOR_H_
